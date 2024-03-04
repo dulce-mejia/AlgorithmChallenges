b8052d204517d3868d064cc4a385c7679528b153
Gardenia Mejia <dgma0607@gmail.com>
2024-03-04 11:16:08 -0600

    tests added

--- AlgorithmChallenges.playground/Pages/PascalTriangle.xcplaygroundpage/Contents.swift
+++ AlgorithmChallenges.playground/Pages/PascalTriangle.xcplaygroundpage/Contents.swift
@@ -1,12 +1,13 @@
 
 import Foundation
+import XCTest
 
 
 let test = 2
 
 printPascalTriangle(test)
 
-func printPascalTriangle(_ rows: Int) {
+func printPascalTriangle(_ rows: Int) -> [[Int]] {
   var triangle: [[Int]] = [[1]]
   print("\(triangle[0])")
   for row in 1..<rows {
@@ -21,5 +22,55 @@ func printPascalTriangle(_ rows: Int) {
     numbers.append(1)
     triangle.append(numbers)
     print("\(triangle[row])")
+  }
+  return triangle
+}
+
+
+PascalTriangleTests.defaultTestSuite.run()
+
+  
+
+// UnitTests Class
+
+class PascalTriangleTests: XCTestCase {
+  
+    var sut: [[Int]]? = nil
+  
+    override func setUp() {
+        super.setUp()
+    }
+  
+    func testOnlyOneRow() {
+      // Given
+      let rows = 1
+      // When
+      let result = printPascalTriangle(rows)
+      // Then
+      XCTAssertEqual(result[0].count, 1)
+    }
+  
+    func testRowsStartsAndEndsInOne() {
+      // Given
+      let randomInt = Int.random(in: 1..<10)
+      // When
+      let result = printPascalTriangle(10)
+      // Then
+      XCTAssertEqual(result[randomInt].first, 1)
+      XCTAssertEqual(result[randomInt].last, 1)
+    }
+    
+    func testNumberOfRowsAccordingInput() {
+        // Given
+        let rows = 5
+        // When
+        let result = printPascalTriangle(rows)
+        // Then
+      XCTAssertEqual(result.count, rows)
+    }
+  
+  override func tearDown() {
+    super.tearDown()
+    sut = nil
   }
 }
